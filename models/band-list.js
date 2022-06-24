3178a36e893d9c9e75dd711598d5be91e66e4f42
robertoweb <roberto.alonso.gandia@gmail.com>
2022-06-24 18:30:33 +0200

    first commit

---
+++ models/band-list.js
@@ -0,0 +1,48 @@
+const Band = require('./band');
+
+class BandList {
+  constructor() {
+    this.bands = [
+      new Band('Metallica'),
+      new Band('Pink Floid'),
+      new Band('Bon Jovi'),
+      new Band('Madonna'),
+    ];
+  }
+
+  addBand(name) {
+    const newBand = new Band(name);
+    this.bands.push(newBand);
+    return this.bands;
+  }
+
+  removeBand(id) {
+    this.bands = this.bands.filter((band) => band.id !== id);
+  }
+
+  getBands() {
+    return this.bands;
+  }
+
+  increaseVotes(id) {
+    this.bands = this.bands.map((band) => {
+      if (band.id === id) {
+        band.votes += 1;
+      }
+
+      return band;
+    });
+  }
+
+  changeName(id, newName) {
+    this.bands = this.bands.map((band) => {
+      if (band.id === id) {
+        band.name = newName;
+      }
+
+      return band;
+    });
+  }
+}
+
+module.exports = BandList;
---
+++ public/precache-manifest.0e9cdf7f3264f819817f21f5a0644a60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a39a6c5fe0aad480b68dda7006c1f29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3459440ef577a4357ead",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "305deed0b8dfa44363bc",
+    "url": "/static/js/2.c6b81791.chunk.js"
+  },
+  {
+    "revision": "f9cac88fa7e1387263857d830e82adef",
+    "url": "/static/js/2.c6b81791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3459440ef577a4357ead",
+    "url": "/static/js/main.da97b3a1.chunk.js"
+  },
+  {
+    "revision": "af071ecaf494e4d66135",
+    "url": "/static/js/runtime-main.53d75a10.js"
+  }
+]);
